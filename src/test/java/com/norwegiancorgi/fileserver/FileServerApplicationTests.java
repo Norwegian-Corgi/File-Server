67f6141e7a6f077324586efb9a12923c6e64cd83
Norwegian-Corgi <83076833+Norwegian-Corgi@users.noreply.github.com>
2022-09-10 20:35:08 +0200

    Initial commit

---
+++ src/test/java/com/norwegiancorgi/fileserver/FileServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.norwegiancorgi.fileserver;
+
+import org.junit.jupiter.api.Disabled;
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@Disabled
+@SpringBootTest
+class FileServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
