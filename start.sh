fcbe6c6a68de3f8a54903c27ee1d961094fc55e2
norwegian-corgi <abdealisabun8@gmail.com>
2024-04-11 21:50:56 +0200

    Update docker compose

--- start.sh
+++ start.sh
@@ -11,12 +11,12 @@ echo "NUXT_PUBLIC_SERVER_IP=$ip" >> "$env_file"
 
 echo "Enter database username:"
 read dbUser
-sed "s/{DB_USER}/$dbUser/g" "$docker_compose_file"
+sed "s/DB_USER/$dbUser/g" "$docker_compose_file"
 echo "DB_USER=$dbUser" >> "$env_file"
 
 echo "Enter database password:"
 read dbPassword
-sed "s/{DB_PASSWORD}/$dbPassword/g" "$docker_compose_file"
+sed "s/DB_PASSWORD/$dbPassword/g" "$docker_compose_file"
 echo "DB_PASSWORD=$dbPassword" >> "$env_file"
 
 echo "DB_HOST=db" >> "$env_file"
@@ -30,4 +30,4 @@ read keycloakPassword
 echo "Starting file server up!"
 
 docker compose up -d
-docker run --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=$keycloakUser -e KEYCLOAK_ADMIN_PASSWORD=$keycloakPassword -v ./keycloak:/opt/keycloak/data/import quay.io/keycloak/keycloak:latest start --import-realm
+docker run --name keycloak -p 8080:8080 -e KEYCLOAK_ADMIN=$keycloakUser -e KEYCLOAK_ADMIN_PASSWORD=$keycloakPassword -v ./keycloak:/opt/keycloak/data/import quay.io/keycloak/keycloak:latest start-dev --import-realm
